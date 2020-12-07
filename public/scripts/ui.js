4a86089fb9eed68f651f999289febc1e431a62be
Dan Zaharia <zaharia.danny@gmail.com>
2020-12-07 10:59:08 -0500

    create item list

--- public/scripts/ui.js
+++ public/scripts/ui.js
@@ -1,5 +1,21 @@
 let readyFunction = function() {
-	console.log(`differe string`);
+	const addItemInput = document.querySelector('.add-item__input');
+	const addItemComponent = document.querySelector('.add-item');
+	const cancelButton = document.querySelector('.add-item__cancel');
+
+	addItemInput.addEventListener('focus', () => {
+		addItemComponent.classList.add('add-item--active');
+	});
+
+	addItemInput.addEventListener('blur', () => {
+		addItemComponent.classList.remove('add-item--active');
+	});
+
+	cancelButton.addEventListener('click', () => {
+		addItemInput.value = '';
+		addItemInput.blur();
+		addItemComponent.classList.remove('add-item--active');
+	});
 }
 
 if (document.readyState != 'loading') {
