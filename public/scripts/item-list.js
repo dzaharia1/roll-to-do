007f351bca7e6c842f0abb7156bb1aed558b3ceb
Dan Zaharia <zaharia.danny@gmail.com>
2021-01-19 16:34:31 -0500

    placeholder todo for item reordering

--- public/scripts/item-list.js
+++ public/scripts/item-list.js
@@ -1,3 +1,32 @@
 readyFunctions.push(() => {
-    const items = document.querySelector
+    const items = document.querySelectorAll('.item-list>li');
+    let touchposition = {
+        x: 0,
+        y: 0
+    };
+
+    // todo: start creating the ability to reorder items
+    // for (thisItem of items) {
+    //     thisItem.addEventListener('touchstart', (e) => {
+    //         if (!e.target.classList.contains('item-list__check')) {
+    //             touchstartHandler(thisItem, e);
+    //         }
+    //     });
+
+    //     thisItem.addEventListener('touchmove', (e) => {
+    //         if (!e.target.classList.contains('item-list__check')) {
+    //             dragItem(thisItem, e);
+    //         }
+    //         // e.preventDefault();
+    //     });
+    // }
 });
+
+function dragItem (item, touchEvent) {
+    console.log(touchEvent);
+}
+
+function touchstartHandler (item, touchEvent) {
+    console.log(touchEvent);
+    touchposition.changedtouches[0]
+}
