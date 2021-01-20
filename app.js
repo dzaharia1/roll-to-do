6adeee8e1c5bf9c7cc6fd533200e2b7792138462
Dan Zaharia <zaharia.danny@gmail.com>
2021-01-19 21:03:26 -0500

    create routes for updating the items list

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ ejs = require('ejs'),
 app = express();
 const localport = '3333';
 const localhost = 'http://localhost';
-let data = require('./data.js');
+let categories = require('./data.js');
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,8 +18,63 @@ app.host = app.set('host', process.env.HOST || localhost);
 app.port = app.set('port', process.env.PORT || localport);
 
 app.get('/', function(req, res) {
-	res.render('index', { data });
+	res.render('index', { categories });
 });
+
+app.put('/addcategory/:category', (req, res) => {
+  addCategory(req.params.category);
+});
+
+app.put('/editcategory/:targetcategory/:newname', (req, res) => {
+  editCategory(req.params.targetcategory, req.params.newname);
+});
+
+app.put('/additem/:targetcategory/:item', (req, res) => {
+  addItem(req.params.targetcategory, req.params.item);
+});
+
+app.put('/editstatus/:targetcategory/:item/:status', (req, res) => {
+  changeItemStatus(req.params.targetcategory, req.params.item, req.params.status);
+});
+
+function addCategory (categoryName) {
+  categories.push({
+    'category': categoryName,
+    'categorySlug': categoryName.replace(/\s/g, ''),
+    'items': []
+  });
+}
+
+function editCategory (targetCategory, newName) {
+  for (let category of categories) {
+    if (category.categorySlug === targetCategory) {
+      category.category = newName;
+    }
+  }
+}
+
+function addItem (targetCategory, item) {
+  for (let category of categories) {
+    if (category.categorySlug === targetCategory) {
+      category.items.push({
+        'name': item,
+        'status': false
+      });
+    }
+  }
+}
+
+function changeItemStatus (targetCategory, targetItem, status) {
+  for (let category of categories) {
+    if (category.categorySlug === targetCategory) {
+      for (let item of category.items) {
+        if (item.name === targetItem) {
+          item.status = status;
+        }
+      }
+    }
+  }
+}
 
 var server = app.listen(app.get('port'), function() {
   app.address = app.get('host') + ':' + server.address().port;
